8f2a26db5b05850e2e27e733f349fb65d1d79c3c
Eric Hung <erichung.94@gmail.com>
2019-04-10 02:44:52 -0500

    Adjusted HTML

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsArray = [
     {
         "name": "Tom Selleck",
         "photo": "https://m.media-amazon.com/images/M/MV5BMTQyMjQzODMzNF5BMl5BanBnXkFtZTcwMjE0NTM1Mw@@._V1_UY317_CR2,0,214,317_AL_.jpg",
-        "score": [
+        "scores": [
             1,
             1,
             1,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,4 @@
 var friendsData = require("../data/friends");
-
 module.exports = function(app) {
     app.get("/api/friends", function (req,res){
         res.json(friendsData);
@@ -11,9 +10,8 @@ app.post("/api/friends", function(req, res){
     var lowestDifference = 50;
     for (i=0;i<friendsData.length;i++){
         var difference = 0;
-
         for (j=0;j<friendScores.length;j++){
-            difference += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(friendScores[j]);    
+            difference += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(friendScores[j]));    
         }
 
         if (difference < lowestDifference){
