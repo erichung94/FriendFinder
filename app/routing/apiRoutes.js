6097fab8745555cb5cfe222f52440ae598b96296
Eric Hung <erichung.94@gmail.com>
2019-04-09 23:51:31 -0500

    playing around with html, still not working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,19 @@
-var path = require('path');
-
 var friendsData = require("../data/friends");
 
-module.exports = function(app){
+module.exports = function(app) {
     app.get("/api/friends", function (req,res){
         res.json(friendsData);
-    })
-};
+    });
+
 
 app.post("/api/friends", function(req, res){
     var friendScores = req.body.scores;
+    var lowestDifference = 50;
     for (i=0;i<friendsData.length;i++){
         var difference = 0;
 
         for (j=0;j<friendScores.length;j++){
-            difference += Math.abs(friendsData[i].scores[j] - friendScores[j]);    
+            difference += Math.abs(parseInt(friendsData[i].scores[j]) - parseInt(friendScores[j]);    
         }
 
         if (difference < lowestDifference){
@@ -24,3 +23,4 @@ app.post("/api/friends", function(req, res){
     friendsData.push(req.body);
     
 });
+};
